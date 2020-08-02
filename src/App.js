bd0ea615c3730808d7fb95dc5738d25e225a71f4
easyphunk <46828328+easyphunk@users.noreply.github.com>
2020-08-02 22:32:52 +0300

    feat: landing page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-import Header from './components/header'
+import Header from './components/header';
+import LandingPage from './pages/landing-page';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <LandingPage />
     </div>
   );
 }
